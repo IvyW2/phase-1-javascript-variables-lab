85a401ee698fdb4160e5c1c2a85ea5195a6bbf0d
IvyW2 <ivywambugha2@gmail.com>
2022-11-22 15:44:52 +0300

    Review: Variables Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber';
+
+const companyName1 = 'Scuber';
+
+const mostProfitableNeighborhood ='Chelsea'
+
+let mostProfitableNeighborhood1 = 'Chels'
+
+const companyCeo = 'Susan Smith'
+
+let companyCeo1= 'Smith Susan'
